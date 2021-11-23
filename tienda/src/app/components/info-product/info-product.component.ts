ffdbf8feebd4c20940f413e16b4d995f0be3842b
xxgdexx <wilmergabrielnegronvelasquez@gmail.com>
2021-11-23 18:32:52 -0500

    IMPLEMENTACIÓN DE CHATBOT V_1

--- tienda/src/app/components/info-product/info-product.component.ts
+++ tienda/src/app/components/info-product/info-product.component.ts
@@ -5,6 +5,26 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './info-product.component.html',
   styleUrls: ['./info-product.component.css']
 })
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+// MOD:WILMERN - CHATBOT
+// export class AppComponent {
+//   title = 'my first app';
+//   ngOnInit() {
+//     (function(d, m){
+//         var kommunicateSettings = 
+//             {"appId":"<YOUR APP-ID>","popupWidget":true,"automaticChatOpenOnNavigation":true};
+//         var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+//         s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+//         var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+//         window.kommunicate = m; m._globals = kommunicateSettings;
+//     })(document, window.kommunicate || {});
+//   }
+// }
 export class InfoProductComponent implements OnInit {
 
   constructor() { }
--- tienda/src/app/components/shop-products/shop-products.component.ts
+++ tienda/src/app/components/shop-products/shop-products.component.ts
@@ -10,7 +10,25 @@ declare var jQuery;
 declare var $;
 declare var noUiSlider:any;
 declare var iziToast;
-
+//MOD:WN -- Implementar chatbot
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.scss']
+// })
+// export class AppComponent {
+//   title = 'my first app';
+//   ngOnInit() {
+//     (function(d, m){
+//         var kommunicateSettings = 
+//             {"appId":"<YOUR APP-ID>","popupWidget":true,"automaticChatOpenOnNavigation":true};
+//         var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+//         s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+//         var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+//         window.kommunicate = m; m._globals = kommunicateSettings;
+//     })(document, window.kommunicate || {});
+//   }
+// }
 @Component({
   selector: 'app-shop-products',
   templateUrl: './shop-products.component.html',
